a85ecc9f128af35d834a9c250f6e9f53fe08b9e9
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2022-08-25 20:41:57 -0300

    Create logic to add or remove favorites in filmcomponent

--- lib/components/filmcomponent.dart
+++ lib/components/filmcomponent.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:teste03_escribo/db/db.dart';
 import '../models/film.dart';
 
 class Films extends StatefulWidget {
@@ -12,6 +13,7 @@ class Films extends StatefulWidget {
 
 class _FilmsState extends State<Films> {
   late Future<List<Film>> futureFilm;
+  late Icon icon;
 
   Future<List<Film>> loadFilms() async {
     final films = await http.get(Uri.parse('https://swapi.dev/api/films'));
@@ -20,10 +22,14 @@ class _FilmsState extends State<Films> {
       // If the server did return a 200 OK response,
       // then parse the JSON.
       List<dynamic> body = jsonDecode(films.body)['results'];
+      var favorites = await DB.getFavoriteFilms();
 
       List<Film> film = body
           .map(
-            (dynamic item) => Film.fromJson(item),
+            (dynamic item) => Film.fromJson(
+                item,
+                favorites
+                    .any((element) => element.values.contains(item["title"]))),
           )
           .toList();
 
@@ -45,23 +51,55 @@ class _FilmsState extends State<Films> {
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: futureFilm,
-      builder: (BuildContext context, AsyncSnapshot<List<Film>> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.hasData) {
-          List<Film> films = snapshot.data ?? <Film>[];
-          return ListView(
-            children: films
-                .map(
-                  (Film post) => Card(
-                    child: ListTile(
-                      title: Text(post.title),
-                      trailing: const IconButton(
-                        icon: Icon(Icons.favorite),
-                        onPressed: null,
-                      ),
-                    ),
+          List<Film> films = snapshot.data;
+
+          return ListView.builder(
+            itemCount: films.length,
+            itemBuilder: (BuildContext context, int id) {
+              if (films[id].isFavorite) {
+                icon = const Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                );
+              } else {
+                icon = const Icon(
+                  Icons.favorite_border,
+                  color: Colors.red,
+                );
+              }
+
+              return Card(
+                child: ListTile(
+                  title: Text(films[id].title),
+                  trailing: IconButton(
+                    icon: icon,
+                    onPressed: () async {
+                      if (films[id].isFavorite) {
+                        DB.deleteFavoriteByName(films[id].title);
+                        setState(() {
+                          films[id].isFavorite = false;
+                          icon = const Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                          );
+                        });
+                      } else {
+                        DB.createFavorite(films[id].title, "film");
+                        setState(() {
+                          films[id].isFavorite = true;
+                          icon = const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          );
+                        });
+                      }
+                    },
                   ),
-                )
-                .toList(),
+                ),
+              );
+            },
           );
         } else {
           return const Center(
