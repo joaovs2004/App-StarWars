fa5ac285b54e16091dd15124bc7724e8cb7adc85
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2022-08-25 20:51:46 -0300

    Add favorites logic on characters component

--- lib/components/characterscomponent.dart
+++ lib/components/characterscomponent.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:teste03_escribo/db/db.dart';
 import '../models/character.dart';
 
 class Charactrs extends StatefulWidget {
@@ -12,17 +13,22 @@ class Charactrs extends StatefulWidget {
 
 class _CharactersState extends State<Charactrs> {
   late Future<List<Character>> futureCharacter;
+  late Icon icon;
 
   Future<List<Character>> loadFilms() async {
-    final characters = await http.get(Uri.parse('https://swapi.dev/api/people'));
+    final characters =
+        await http.get(Uri.parse('https://swapi.dev/api/people'));
 
     if (characters.statusCode == 200) {
       // If the server did return a 200 OK response,
       // then parse the JSON.
       List<dynamic> body = jsonDecode(characters.body)['results'];
+      var favorites = await DB.getFavoriteCharacters();
+
       List<Character> character = body
           .map(
-            (dynamic item) => Character.fromJson(item),
+            (dynamic item) => Character.fromJson(item, favorites
+                .any((element) => element.values.contains(item["name"]))),
           )
           .toList();
 
@@ -44,23 +50,55 @@ class _CharactersState extends State<Charactrs> {
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: futureCharacter,
-      builder: (BuildContext context, AsyncSnapshot<List<Character>> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.hasData) {
-          List<Character> films = snapshot.data ?? <Character>[];
-          return ListView(
-            children: films
-                .map(
-                  (Character post) => Card(
-                    child: ListTile(
-                      title: Text(post.name),
-                      trailing: const IconButton(
-                        icon: Icon(Icons.favorite),
-                        onPressed: null,
-                      ),
-                    ),
+          List<Character> characters = snapshot.data;
+
+          return ListView.builder(
+            itemCount: characters.length,
+            itemBuilder: (BuildContext context, int id) {
+              if (characters[id].isFavorite) {
+                icon = const Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                );
+              } else {
+                icon = const Icon(
+                  Icons.favorite_border,
+                  color: Colors.red,
+                );
+              }
+
+              return Card(
+                child: ListTile(
+                  title: Text(characters[id].name),
+                  trailing: IconButton(
+                    icon: icon,
+                    onPressed: () async {
+                      if (characters[id].isFavorite) {
+                        DB.deleteFavoriteByName(characters[id].name);
+                        setState(() {
+                          characters[id].isFavorite = false;
+                          icon = const Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                          );
+                        });
+                      } else {
+                        DB.createFavorite(characters[id].name, "character");
+                        setState(() {
+                          characters[id].isFavorite = true;
+                          icon = const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          );
+                        });
+                      }
+                    },
                   ),
-                )
-                .toList(),
+                ),
+              );
+            },
           );
         } else {
           return const Center(
